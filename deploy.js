c3eafd909a53512eb1b4370172d712b84cd5f1bf
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-05-21 21:52:44 -0400

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,11 +1,11 @@
-async function deploy() {
-  const fs = require("node:fs");
-  const path = require("node:path");
-  const { REST } = require("@discordjs/rest");
-  const { Routes } = require("discord.js");
-  const { clientId, token } = require("./Storage/config.json");
-  const constantsFile = require("./Storage/constants");
+const fs = require("node:fs");
+const path = require("node:path");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord.js");
+const { clientId, token } = require("./Storage/config.json");
+const constantsFile = require("./Storage/constants");
 
+async function deploy() {
   const commands = [];
   const commandsPath = path.join(__dirname, "Commands");
   const commandsFolder = fs.readdirSync("./Commands");
@@ -19,28 +19,23 @@ async function deploy() {
       commands.push(command.data.toJSON());
     }
   }
+
   const rest = new REST({ version: "10" }).setToken(token);
 
-  // Delete all commands
-  rest
-    .get(Routes.applicationGuildCommands(clientId, constantsFile.mainServerID))
-    .then((data) => {
-      const promises = [];
-      for (const command of data) {
-        const deleteUrl = `${Routes.applicationGuildCommands(clientId, constantsFile.mainServerID)}/${command.id}`;
-        promises.push(rest.delete(deleteUrl));
-      }
-      return Promise.all(promises);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  try {
+    // Delete all commands
+    const existingCommands = await rest.get(Routes.applicationGuildCommands(clientId, constantsFile.mainServerID));
+    const deletePromises = existingCommands.map((command) =>
+      rest.delete(`${Routes.applicationGuildCommands(clientId, constantsFile.mainServerID)}/${command.id}`)
+    );
+    await Promise.all(deletePromises);
 
-  // Add all the commands back
-  rest
-    .put(Routes.applicationCommands(clientId), { body: commands })
-    .then(() => console.log("Successfully registered application commands."))
-    .catch(console.error);
+    // Add all the commands back
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
+    console.log("Successfully registered application commands.");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = { deploy };
