4241cac9ebadbb9487a73f70f7e58f567bab8016
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-05-23 16:01:07 -0400

    Update pause.js

--- Commands/Music/pause.js
+++ Commands/Music/pause.js
@@ -16,7 +16,7 @@ module.exports = {
     if (!queue) return interaction.reply({ content: `no music currently playing `, ephemeral: true });
 
     if (queue.connection.paused) return interaction.reply({ content: "the track is currently paused!", ephemeral: true });
-
-    return inter.reply({ content: `${queue.current.title} paused ✅` });
+    queue.setPaused(true);
+    return interaction.reply({ content: `${queue.current.title} paused` });
   },
 };
