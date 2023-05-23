cd437fdd55d66de035d00dd59f87330df58c6209
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-05-23 14:35:03 -0400

    Create pause.js

---
+++ Commands/Music/pause.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("pause").setDescription("Pause a song"),
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    const player = interaction.client.player;
+    const channel = interaction.member.voice.channel;
+
+    if (!channel) {
+      return interaction.reply("you are not in a voice channel!");
+    }
+    const queue = player.nodes.get(interaction.guildId);
+
+    if (!queue) return interaction.reply({ content: `no music currently playing `, ephemeral: true });
+
+    if (queue.connection.paused) return interaction.reply({ content: "the track is currently paused!", ephemeral: true });
+
+    return inter.reply({ content: `${queue.current.title} paused ✅` });
+  },
+};
