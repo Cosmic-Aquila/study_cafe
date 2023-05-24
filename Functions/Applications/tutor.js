b7ad9c483434cfa4d2bb328dd1f943b69c763e54
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-05-24 12:42:02 -0400

    update tutor application

--- Functions/Applications/tutor.js
+++ Functions/Applications/tutor.js
@@ -11,14 +11,22 @@ async function tutorApplication(message) {
     const questionArray = [
       "Age? (Can be a range like 13-15 if you are not comfortable sharing)",
       "Timezone?",
-      "What is your current grade?",
-      "Tell us about yourself!",
-      "How many hours a week can you be on?",
-      "Why do you want to join the study cafe tutor team?",
+      "What is your current grade level?",
+      "Have you ever been a Homework Helper or tutor in any other capacity? If yes, please provide details.",
+      "What subjects or areas are you most knowledgeable in and comfortable assisting with?",
+      "What motivates you to help others with their homework and academic challenges?",
       "Are you a member of staff or a tutor in any other servers right now? If so list the names and member count. If not, put N/A.",
-      "Do you have any tutor experience? Please explain in detail. If not, put N/A.",
-      "In one message, Please list the your chosen tutor topics. E.G. Maths, English, Science, ETC.",
-      "For the topics you chose; please list, in one message, the modules you know the most about. E.G. Maths - Calc & Trig OR Science - Physics",
+      "How familiar are you with our server's guidelines and rules?",
+      "How would you handle a situation where a student asks for answers instead of seeking guidance or understanding?",
+      "Describe your approach to helping students who are struggling with a difficult concept or topic.",
+      "Have you ever encountered a student who consistently relies on others to complete their assignments without making an effort to learn? If so, how did you handle it?",
+      "How would you handle a situation where a student becomes disrespectful or rude towards you or another member of the server?",
+      "Are you able to explain complex ideas or concepts in a clear and understandable manner?",
+      "How active are you on Discord, and how much time can you dedicate to helping students on our server?",
+      "Are you open to receiving feedback from others and improving your own tutoring skills?",
+      "Do you have any additional skills or experience that you believe would benefit our team?",
+      "How would you handle sensitive or confidential information that a student might share with you?",
+      "Is there anything else you would like to add to support your application?",
     ];
 
     // change type to tutor
@@ -41,7 +49,9 @@ async function tutorApplication(message) {
   } else if (applicationData.answers.length == applicationData.questions.length - 1) {
     applicationData.answers.push(message.content);
     applicationData.save();
-    message.channel.send("Application done!");
+    message.channel.send(
+      "Thank you for taking the time to complete this tutor application. We appreciate your interest in joining our team. We will carefully review your responses and contact you if we find that you are a good fit for the role. Best of luck!"
+    );
     const guild = await message.client.guilds.fetch(constantsFile.staffServerID);
     const channel = await guild.channels.fetch(constantsFile.applicationLogChannel);
     const levelData = await levelModel.findOne({ memberID: message.author.id });
