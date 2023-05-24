4f65889eb7c32acf0dda51cdb43cdc103f5b3fb2
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-05-24 12:37:56 -0400

    update application questions

--- Functions/Applications/mod.js
+++ Functions/Applications/mod.js
@@ -11,19 +11,22 @@ async function modApplication(message) {
     const questionArray = [
       "Age? (Can be a range like 13-15 if you are not comfortable sharing)",
       "Timezone?",
-      "Tell me about yourself!",
-      "How many hours a week can you be on?",
-      "Why do you want to be on the moderation team?",
-      "Are you staff in any other servers right now? If so list the names and member count. If not, put N/A.",
-      "Do you have any staff experience? Please explain in detail. If not, put N/A.",
-      "To be a moderator we are currently requiring 50 messages a month. Please note this is subject to change as the server grows. We will never require more than 100 even when we are a large server. Respond with anything to agree to the terms.",
-      "If someone was using bot commands in lounge what would you do?",
-      "If someone was cussing at other members what would you do?",
-      "If another staff member was abusing their power what would you do?",
-      "If someone posts potentially triggering/NSFW content what would you do?",
-      "If someone was impersonating staff what would you do?",
-      "If someone was mini-modding what would you do?",
-      "If someone is begging for higher roles or staff what would you do?",
+      "Have you ever been a moderator in any other Discord server? If yes, please provide details.",
+      "Why do you want to become a moderator for our server?",
+      "What qualities or skills do you possess that make you suitable for a moderation role?",
+      "How familiar are you with our server's rules and guidelines?",
+      "How would you handle a situation where two members are engaged in a heated argument?",
+      "What would you do if you witnessed someone breaking the server rules?",
+      "How would you approach a member who consistently disrupts the server's conversations?",
+      "Have you ever encountered a conflict of interest between your personal relationships with server members and your responsibilities as a moderator? If so, how did you handle it?",
+      "How active are you on Discord, and how much time can you dedicate to moderating our server?",
+      "Do you have any experience with moderation bots or tools? If yes, please provide details.",
+      "Are you comfortable enforcing rules and issuing appropriate warnings or penalties when necessary?",
+      "How would you contribute to maintaining a positive and inclusive environment within our server?",
+      "Have you ever faced criticism or pushback from others regarding your moderation decisions? How did you handle it?",
+      "Do you have any additional skills or experience that you believe would benefit our server's moderation team?",
+      "How would you handle confidential or sensitive information if it were to come to your attention as a moderator?",
+      "Is there anything else you would like to add to support your application?",
     ];
     const questionData = new applicationModel({
       questions: questionArray,
@@ -44,7 +47,9 @@ async function modApplication(message) {
   } else if (applicationData.answers.length == applicationData.questions.length - 1) {
     applicationData.answers.push(message.content);
     applicationData.save();
-    message.channel.send("Application done!");
+    message.channel.send(
+      "Thank you for taking the time to complete this moderator application. We appreciate your interest in joining our moderation team. We will carefully review your responses and contact you if we find that you are a good fit for the role. Good luck!"
+    );
     const guild = await message.client.guilds.fetch(constantsFile.staffServerID);
     const channel = await guild.channels.fetch(constantsFile.applicationLogChannel);
     const levelData = await levelModel.findOne({ memberID: message.author.id });
