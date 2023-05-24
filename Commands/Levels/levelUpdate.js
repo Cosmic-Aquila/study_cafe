5d096ee73cbce09396e6bc7f2ee0f6d4b1e515af
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-05-24 14:44:51 -0400

    Create levelUpdate.js

---
+++ Commands/Levels/levelUpdate.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require("discord.js");
+const expModel = require("../../Model/Levels/exp.js");
+const { checkLevel } = require("../../Functions/Levels/checkLevel.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("levelupdate")
+    .setDescription("Update everyone's levels.")
+    .addUserOption((option) => option.setName("target").setDescription("The user to check")),
+
+  async execute(interaction) {
+    try {
+      const members = await interaction.guild.members.fetch();
+
+      members.forEach(async (member) => {
+        const data = await expModel.find({ memberID: member.id });
+        checkLevel(data.level, interaction.guild, member);
+      });
+
+      interaction.reply({ content: "Done", ephemeral: true });
+    } catch (error) {
+      console.error("Error fetching guild members:", error);
+    }
+  },
+};
