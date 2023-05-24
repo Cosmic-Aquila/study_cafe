b73cc37415a28a71c8cdd41c8b19743aea9ce189
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-05-24 14:30:20 -0400

    Update autoreply.js

--- Commands/Staff/autoreply.js
+++ Commands/Staff/autoreply.js
@@ -1,11 +1,10 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
-const constantsFile = require("../../Storage/constants.js");
 const autoReply = require("../../Model/replies.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("eval")
-    .setDescription("Evaluate Code!")
+    .setName("autoreply")
+    .setDescription("Add an auto-reply!")
     .addStringOption((option) => option.setName("trigger").setDescription("The trigger").setRequired(true))
     .addStringOption((option) => option.setName("reply").setDescription("The response").setRequired(true))
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
