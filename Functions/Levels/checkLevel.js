a6a5503d339c1e9ccc150200764dd23bbb93a714
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-05-26 19:11:30 -0400

    Update checkLevel.js

--- Functions/Levels/checkLevel.js
+++ Functions/Levels/checkLevel.js
@@ -1,25 +1,31 @@
 const constantsFile = require("../../Storage/constants.js");
 
 async function checkLevel(level, guild, member) {
-  if (level === 1) {
+  if (level >= 1) {
     let role = await guild.roles.fetch(constantsFile.levelonerole);
     member.roles.add(role);
-  } else if (level === 2) {
+  }
+  if (level >= 2) {
     let role = await guild.roles.fetch(constantsFile.leveltworole);
     member.roles.add(role);
-  } else if (level === 3) {
+  }
+  if (level >= 3) {
     let role = await guild.roles.fetch(constantsFile.levelthreerole);
     member.roles.add(role);
-  } else if (level === 20) {
+  }
+  if (level >= 20) {
     let role = await guild.roles.fetch(constantsFile.leveltwentyrole);
     member.roles.add(role);
-  } else if (level === 30) {
+  }
+  if (level >= 30) {
     let role = await guild.roles.fetch(constantsFile.levelthirtyrole);
     member.roles.add(role);
-  } else if (level === 40) {
+  }
+  if (level >= 40) {
     let role = await guild.roles.fetch(constantsFile.levelfortyrole);
     member.roles.add(role);
-  } else if (level === 50) {
+  }
+  if (level >= 50) {
     let role = await guild.roles.fetch(constantsFile.levelfiftyrole);
     member.roles.add(role);
   }
