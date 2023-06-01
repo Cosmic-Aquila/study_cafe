df54abae8995793bbc9b209e5361535d41871ed4
Lauren <134239544+Cosmic-Aquila@users.noreply.github.com>
2023-06-01 15:41:25 -0400

    Create pointsLB.js

---
+++ Commands/Pomodoro/pointsLB.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const pointsModel = require("../../Model/Pomodoro/minutes.js");
+const ordinal = (num) => `${num.toLocaleString("en-US")}${[, "st", "nd", "rd"][(num / 10) % 10 ^ 1 && num % 10] || "th"}`;
+const constantsFile = require("../../Storage/constants.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("pointslb").setDescription("Check the pomdoro lb."),
+  async execute(interaction) {
+    const lb = await pointsModel.find({}).sort({ points: -1 }).limit(10);
+    const guild = await interaction.client.guilds.fetch(constantsFile.mainServerID);
+    if (lb.length === 0) {
+      interaction.reply("No Data!");
+    }
+
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: "study cafe pomodoro lb",
+        iconURL: guild.iconURL({ extension: "png" }),
+      })
+      .setColor("#eec1ad");
+
+    for (let i = 0; i < lb.length; i++) {
+      const memberID = lb[i].memberID;
+      const name = await guild.members
+        .fetch(memberID)
+        .then((member) => member.user.username)
+        .catch(() => null);
+      if (name) {
+        embed.addFields({ name: `${ordinal(i + 1)}. ${name}`, value: `**points:** ${lb[i].points}` });
+      }
+    }
+
+    return interaction.reply({ embeds: [embed] });
+  },
+};
